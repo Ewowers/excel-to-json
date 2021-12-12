e2b830617d177108a928884056d28adf19bcdabe
Ewowers <84778797+Ewowers@users.noreply.github.com>
2021-12-13 02:04:44 +0600

    1

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 "use strict";
 import excelToJson from "convert-excel-to-json";
-const result = (path) => excelToJson({ sourceFile: path });
-console.log(result("./ex.xlsx"));
+import express from "express";
+import path from "path";
+const result = (path) => excelToJson({ sourceFile: path }); // парсинг
+const app = express();
+app.get("/api/excel/:id", (req, res) => {
+  const { id } = req.params; //название файла
+  const excel = path.join("excels", id + ".xlsx"); // пусть к файлу
+  res.json(result(excel)); // отдает json
+});
+app.listen(5000);
